912084b943b9511c9c99ac5554605b6931a22b9a
ThanhTung <thanhtung@gmail.com>
2022-04-05 22:35:30 +0700

    remake Day1

--- Day1/main.js
+++ Day1/main.js
@@ -1,10 +1,9 @@
-var panels = document.querySelectorAll('.panel');
-console.log(panels);
-for(let panel of panels) {
-    panel.addEventListener('click',() => {
-        for(let child of panels){
-            child.classList.remove('active')
+var panels = document.getElementsByClassName ("panel");
+for (let panel of panels) {
+    panel.addEventListener('click', () => {
+        for(let child of panels) {
+            child.classList.remove ("active");
         }
-        panel.classList.add('active');
+        panel.classList.add ("active"); 
     })
 }
